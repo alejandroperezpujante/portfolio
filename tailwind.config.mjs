b1c522719df90267d1e19f726370b55bf0bb2fdb
Alejandro Perez Pujante <perezpujantealejandro1@gmail.com>
2024-08-13 18:34:51 +0200

    minimal SEO + theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      screens: {
+        "high-dpi": {
+          raw: "only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (min-resolution: 192dpi)",
+        },
+			},
+			colors: {
+				background: "oklch(var(--background) / <alpha-value>)",
+				foreground: "oklch(var(--foreground) / <alpha-value>)",
+			}
+    },
+  },
+  plugins: [],
+};
