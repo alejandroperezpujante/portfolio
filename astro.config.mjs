f2c53c4f6c5ef3a81d0fe11f3bf85f6a8a549c2b
Alejandro Perez Pujante <perezpujantealejandro1@gmail.com>
2024-08-14 10:56:42 +0200

    add sitemap generation and compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,22 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import compress from "astro-compress";
 import icons from "unplugin-icons/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://portfolio.perezpujantealejandro.dev",
   integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
+    tailwind({ applyBaseStyles: false }),
+		sitemap(),
+		compress()
   ],
   vite: {
-    plugins: [icons({ compiler: "astro" })],
+    plugins: [
+      icons({
+        compiler: "astro",
+      }),
+    ],
   },
 });
