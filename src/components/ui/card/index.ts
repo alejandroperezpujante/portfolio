2412231969607107ffb2795c1bfacde2017e0700
Alejandro Perez Pujante <perezpujantealejandro1@gmail.com>
2024-08-22 19:53:04 +0200

    Allow class as props for card and handle tailwind class conflicts

--- src/components/ui/card/index.ts
+++ src/components/ui/card/index.ts
@@ -1,16 +1,22 @@
 import Root from "./Card.astro";
 import Header from "./CardHeader.astro";
 import Title from "./CardTitle.astro";
+import Description from "./CardDescription.astro";
 import Content from "./CardContent.astro";
+import Footer from "./CardFooter.astro";
 
 export {
 	Root,
 	Header,
 	Title,
+	Description,
 	Content,
+	Footer,
 
 	Root as Card,
 	Header as CardHeader,
 	Title as CardTitle,
-	Content as CardContent
+	Description as CardDescription,
+	Content as CardContent,
+	Footer as CardFooter
 };
